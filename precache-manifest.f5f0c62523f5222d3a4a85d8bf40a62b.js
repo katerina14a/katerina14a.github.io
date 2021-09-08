fba3a49be4081e92f0bb77c1a3fb5d57b756b7d1
kat <kataleksandrova@gmail.com>
2021-09-08 09:30:33 -0700

    deploy

---
+++ precache-manifest.f5f0c62523f5222d3a4a85d8bf40a62b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4032e94d6f44c2965af",
+    "url": "/css/app.0cbf298b.css"
+  },
+  {
+    "revision": "b4032e94d6f44c2965af",
+    "url": "/js/app.268e13f3.js"
+  },
+  {
+    "revision": "4e62ac76604e96a2d72e",
+    "url": "/js/chunk-vendors.1a1fa139.js"
+  },
+  {
+    "revision": "9b541d1c8812a3af1d55431c2929856b",
+    "url": "/img/flowers_left.9b541d1c.png"
+  },
+  {
+    "revision": "42e297b1da70a9d338181d2520bc1809",
+    "url": "/img/flowers_right.42e297b1.png"
+  },
+  {
+    "revision": "afd1eb17097bc0f2176534cd6f9c99fd",
+    "url": "/img/loading_cat.afd1eb17.gif"
+  },
+  {
+    "revision": "3e718128994284b69bf75a0f1ba3735a",
+    "url": "/img/kat-danny-engagement-110.3e718128.jpg"
+  },
+  {
+    "revision": "07330ec02d1bf3ef707e9c14e2acfb23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b541d1c8812a3af1d55431c2929856b",
+    "url": "/img/flowers_left.png"
+  },
+  {
+    "revision": "42e297b1da70a9d338181d2520bc1809",
+    "url": "/img/flowers_right.png"
+  },
+  {
+    "revision": "afd1eb17097bc0f2176534cd6f9c99fd",
+    "url": "/img/loading_cat.gif"
+  },
+  {
+    "revision": "b2328b4e9fbe250f9d7b7df8add3a88f",
+    "url": "/img/peachy-flowers-3.png"
+  },
+  {
+    "revision": "879f9f5725826f57c00d8f514fc470c1",
+    "url": "/img/peachy-flowers-1.png"
+  },
+  {
+    "revision": "95643f1b209851253900a0532287b598",
+    "url": "/img/peachy-flowers-2.png"
+  },
+  {
+    "revision": "f4640ff25d7708219993a646ab67fde4",
+    "url": "/img/peachy-flowers-5.png"
+  },
+  {
+    "revision": "009d7be347ecb82df8194cca3154c1a0",
+    "url": "/img/peachy-flowers-6.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "674910889c43045c6302687a0abe7f85",
+    "url": "/img/peachy-flowers-bg.jpg"
+  },
+  {
+    "revision": "4b3b0671bb2e648d7e66ff3616f02bd2",
+    "url": "/img/peachy-flowers-4.png"
+  },
+  {
+    "revision": "af885dde2f2166d8a89701df205e6d9b",
+    "url": "/img/peachy-flowers-1.tif"
+  }
+];
